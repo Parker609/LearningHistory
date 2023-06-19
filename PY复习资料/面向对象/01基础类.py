26bd79b87676b959ecd4c9853876298dce70263a
parker609 <673618143@qq.com>
2023-06-20 00:37:00 +0800

    不知所谓

--- PY复习资料/面向对象/01基础类.py
+++ PY复习资料/面向对象/01基础类.py
@@ -9,11 +9,21 @@
 # python的对象实例化竟然不需要new，不用new关键字；
 
 class Ani:
+    # 对象的属性
+    name = "cat"
+    # 在python3.5增加了返回值类型，算是提高了代码的可读性，降低了阅读难度；
     def __init__(self) -> None:
         pass
-
+    
+    # 对象的方法
     def bark(self):
         print('bark')
 
 ani = Ani()
 ani.bark()
+print(ani.name)
+
+"""
+    二、类的属性和方法
+        1. 属性：静态属性、对象属性；
+"""
