fb0d0ede1b5199d186365e3b2add03725b92bcc4
parker609 <673618143@qq.com>
2023-07-13 01:15:57 +0800

    webpack配置文件的设定

---
+++ TS复习资料/webpack.config.js
@@ -0,0 +1,32 @@
+// webpack 的配置包
+
+const path = require('path'); // node的path包引入
+
+// webpack的配置文件都写在momdule.exports中；
+
+module.exports = {
+    // 指定入口文件
+    entry: "./src/index.ts",
+
+    // 指定打包后输出文件目录
+    output: {
+        // 路径
+        path: path.resolve(__dirname, 'dist'),
+        // 文件名
+        filename: "bundle.js"
+    },
+    // 指定webpack打包的时候使用规则
+    module: {
+        // 打包规则
+        rules: [
+            {
+                // test指定规则生效文件
+                test: /\.ts/, // 正则表达式表示对哪些文件生效
+                // 使用ts-loader去处理
+                use: 'te-loader',
+                // 要排除的文件，即不处理的文件
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
